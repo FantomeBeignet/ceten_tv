7b0dfbee43830722e4b444ec6bf7a01cfe453f5e
BENE Tom <tom.bene@telecomnancy.eu>
2022-02-15 12:33:39 +0100

    Fixed tailwind dark selector I forgot

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
-    <nav className="flex items-center justify-start h-16 px-4 py-3 shadow-sm dark:bg-gray-900/50">
+    <nav className="flex items-center justify-start h-16 px-4 py-3 shadow-sm bg-gray-900/50">
       <Link to="/admin">
         <div className="flex items-center justify-start w-fit">
           <img src="/ceten.png" alt="Le logo du CETEN" height="40" width="40" />
