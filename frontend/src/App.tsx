0e44af231efb36b95e42882906840bfcf0c3fc91
BENE Tom <tom.bene@telecomnancy.eu>
2022-03-09 14:40:40 +0100

    Moved whole page to /admin location

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,22 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import ImageList from "./components/ImageList";
 
 export default function App() {
   return (
     <Router>
       <div id="wrapper" className="min-h-screen bg-gray-800">
         <Routes>
-          <Route path="/admin" element={<Navbar />} />
+          <Route
+            path="/admin"
+            element={
+              <>
+                <Navbar />
+                <ImageList />
+              </>
+            }
+          />
         </Routes>
       </div>
     </Router>
