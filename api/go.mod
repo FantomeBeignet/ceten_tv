d06f21bac2f1dadd21b61024e24010fd897b61ca
BENE Tom <tom.bene@telecomnancy.eu>
2022-03-09 14:39:48 +0100

    Added dotenv for images folder location and added file upload function

--- api/go.mod
+++ api/go.mod
@@ -5,3 +5,5 @@ go 1.17
 require github.com/julienschmidt/httprouter v1.3.0
 
 require github.com/rs/cors v1.8.2
+
+require github.com/joho/godotenv v1.4.0 // indirect
--- api/main.go
+++ api/main.go
@@ -12,11 +12,11 @@ import (
 
 	"github.com/julienschmidt/httprouter"
 	"github.com/rs/cors"
+	"github.com/joho/godotenv"
 )
 
 
 func listImages() []string {
-	fmt.Printf("%s/images\n", os.Getenv("REACT_PUBLIC_DIRECTORY"))
 	files, err := ioutil.ReadDir(fmt.Sprintf("%s/images", os.Getenv("REACT_PUBLIC_DIRECTORY")))
     if err != nil {
         log.Fatal(err)
@@ -39,9 +39,28 @@ func AllImages(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	fmt.Fprint(w, string(jsonData))
 }
 
-func main() {
+func DeleteImage(w http.ResponseWriter, r*http.Request, ps httprouter.Params) {
+	filename := ps.ByName("filename")
+	_, err := os.Stat(fmt.Sprintf("%s/images/%s", os.Getenv("REACT_PUBLIC_DIRECTORY"), filename))
+	if err != nil {
+		http.Error(w, "The file does not exist", http.StatusNotFound)
+	} else {
+		err := os.Rename(fmt.Sprintf("%s/images/%s", os.Getenv("REACT_PUBLIC_DIRECTORY"), filename), fmt.Sprintf("%s/images/old/%s", os.Getenv("REACT_PUBLIC_DIRECTORY"), filename))
+		if err == nil {
+			json, _ := json.MarshalIndent(map[string]bool{"ok": true}, "", "\t")
+			fmt.Print(w, string(json))
+		}
+	}
+}
+
+func main() {	
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 	router := httprouter.New()
 	router.GET("/api/images", AllImages)
+	router.GET("/api/delete/:filename", DeleteImage)
 	handler := cors.Default().Handler(router)
 
 	log.Fatal(http.ListenAndServe(":8080", handler))
