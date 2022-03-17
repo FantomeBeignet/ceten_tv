c8e8061fde896f9672b83cccb4c1d47197e30d93
BENE Tom <tom.bene@telecomnancy.eu>
2022-03-17 16:55:46 +0100

    Added carousel to root url

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,12 +2,15 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ImageListPage from "./pages/ImageListPage";
 import SingleImagePage from "./pages/SingleImagePage";
+import Carousel from "./components/Carousel";
 
 export default function App() {
   return (
     <Router>
       <div id="wrapper" className="min-h-screen bg-gray-800">
         <Routes>
+          <Route path="/" element={<Carousel />} />
+
           <Route path="/admin" element={<ImageListPage />} />
           <Route
             path="/admin/images/:imagename"
