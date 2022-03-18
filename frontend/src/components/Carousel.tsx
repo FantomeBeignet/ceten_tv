b7eedb7d2b29093b3c796fa6440b4c60bc190637
BENE Tom <tom.bene@telecomnancy.eu>
2022-03-18 11:01:26 +0100

    Images now switches with set time interval

--- frontend/src/components/Carousel.tsx
+++ frontend/src/components/Carousel.tsx
@@ -2,28 +2,37 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 
 const { REACT_APP_SERVER_IP } = process.env;
+const INTERVAL = 8000;
 
 export default function Carousel() {
   const [loading, setLoading] = useState(true);
   const [images, setImages] = useState([]);
   const [currentIndex, setCurrentIndex] = useState(0);
 
-  useEffect(() => {
+  function fetchImages() {
     axios.defaults.baseURL = `http://${REACT_APP_SERVER_IP}:8080`;
     axios.get("/api/images").then((response) => {
       setImages(response.data);
       setLoading(false);
     });
-  }, []);
+  }
 
-  console.log(images);
+  useEffect(() => {
+    if (currentIndex === 0) {
+      fetchImages();
+    }
+    const interval = setInterval(() => {
+      setCurrentIndex((currentIndex + 1) % images.length);
+    }, INTERVAL);
+    return () => clearInterval(interval);
+  }, [currentIndex, images.length]);
 
   return (
     <>
       {loading ? (
-        <span>coucou</span>
+        <></>
       ) : (
-        <div className="flex items-center justify-center">
+        <div key={currentIndex} className="flex items-center justify-center">
           <img
             src={require(`../../public/images/${images[currentIndex]}`)}
             alt=""
