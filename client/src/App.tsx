f14bdc55d7d6445b2e51bd4749fd0b537ac108e7
BENE Tom <tom.bene@telecomnancy.eu>
2022-05-05 18:47:10 +0200

    Renamings for consistency

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import ImageListPage from "./pages/ImageListPage";
+import AdminPage from "./pages/AdminPage";
 import SingleImagePage from "./pages/SingleImagePage";
 import Carousel from "./components/Carousel";
 
@@ -10,7 +10,7 @@ export default function App() {
       <div id="wrapper" className="min-h-screen bg-gray-800">
         <Routes>
           <Route path="/" element={<Carousel />} />
-          <Route path="/admin" element={<ImageListPage />} />
+          <Route path="/admin" element={<AdminPage />} />
           <Route
             path="/admin/images/:imagename"
             element={<SingleImagePage />}
