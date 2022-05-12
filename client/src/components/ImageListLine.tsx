af2fcbf3989370cdcc5ed151f9487887a06367ef
BENE Tom <tom.bene@telecomnancy.eu>
2022-05-12 12:59:54 +0200

    Modified links to match

--- client/src/components/ImageListLine.tsx
+++ client/src/components/ImageListLine.tsx
@@ -13,12 +13,12 @@ export default function ImageListLine(
       className="hover:bg-slate-700 border-y-2 border-slate-700"
     >
       <td className="relative w-4/12 px-2 py-2 lg:px-6 lg:py-4">
-        <Link to={`/admin/images/${imageName}`}>
+        <Link to={`/admin/${imageName}`}>
           <img src={`/api/image/${imageName}`} alt={displayName} />
         </Link>
       </td>
       <td className="w-10 px-2 py-2 text-xs text-left text-white truncate lg:text-2xl lg:px-6 lg:py-4">
-        <Link to={`/admin/images/${imageName}`}>
+        <Link to={`/admin/${imageName}`}>
           <span className="hover:underline">{displayName}</span>
         </Link>
       </td>
