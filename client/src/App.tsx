825ffc80b3e5cef282ee31fc6c12c2947b246c79
BENE Tom <tom.bene@telecomnancy.eu>
2022-05-12 12:11:14 +0200

    Added global login context

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,22 +1,30 @@
-import React from "react";
+import React, { createContext, useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AdminPage from "./pages/AdminPage";
 import Carousel from "./components/Carousel";
 import SingleImagePage from "./pages/SingleImagePage";
 
+export const LoginContext = createContext({
+  loggedIn: false,
+  setLoggedIn: (state: boolean) => {},
+});
+
 export default function App() {
+  const [loggedIn, setLoggedIn] = useState(false);
   return (
-    <BrowserRouter>
-      <div id="wrapper" className="min-h-screen bg-gray-800">
-        <Routes>
-          <Route path="/" element={<Carousel />} />
-          <Route path="/admin" element={<AdminPage />} />
-          <Route
-            path="/admin/images/:imagename"
-            element={<SingleImagePage />}
-          />
-        </Routes>
-      </div>
-    </BrowserRouter>
+    <LoginContext.Provider value={{ loggedIn, setLoggedIn }}>
+      <BrowserRouter>
+        <div id="wrapper" className="min-h-screen bg-gray-800">
+          <Routes>
+            <Route path="/" element={<Carousel />} />
+            <Route path="/admin" element={<AdminPage />} />
+            <Route
+              path="/admin/images/:imagename"
+              element={<SingleImagePage />}
+            />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </LoginContext.Provider>
   );
 }
