4ba7270d3b9e29a68ed00bb92c4775f4dc285caf
BENE Tom <tom.bene@telecomnancy.eu>
2022-05-12 12:11:34 +0200

    Implemented login on admin page

--- client/src/pages/AdminPage.tsx
+++ client/src/pages/AdminPage.tsx
@@ -1,11 +1,25 @@
+import React, { useContext } from "react";
 import Navbar from "../components/Navbar";
 import ImageList from "../components/ImageList";
+import LoginButton from "../components/LoginButton";
+import { LoginContext } from "../App";
+import LogoutButton from "../components/LogoutButton";
 
 export default function AdminPage() {
+  const { loggedIn } = useContext(LoginContext);
   return (
     <>
       <Navbar />
-      <ImageList />
+      {loggedIn ? (
+        <>
+          <ImageList />
+          <div className="flex items-center justify-center w-full my-16">
+            <LogoutButton />
+          </div>
+        </>
+      ) : (
+        <LoginButton />
+      )}
     </>
   );
 }
