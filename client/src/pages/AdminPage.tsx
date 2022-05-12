ff5aab49d5465556bae318c2372d454e1de7600b
BENE Tom <tom.bene@telecomnancy.eu>
2022-05-12 12:59:33 +0200

    Refactored admin page to use a single component and have better login system

--- client/src/pages/AdminPage.tsx
+++ client/src/pages/AdminPage.tsx
@@ -4,22 +4,40 @@ import ImageList from "../components/ImageList";
 import LoginButton from "../components/LoginButton";
 import { LoginContext } from "../App";
 import LogoutButton from "../components/LogoutButton";
+import { useParams } from "react-router-dom";
+import TopButtonBar from "../components/TopButtonBar";
+import Image from "../components/Image";
+import SingleDeleteButton from "../components/SingleDeleteButton";
 
 export default function AdminPage() {
   const { loggedIn } = useContext(LoginContext);
+  const { imagename } = useParams();
+  var component = null;
+  if (imagename !== undefined) {
+    const displayName = imagename.split(".")[0];
+    component = (
+      <>
+        <TopButtonBar
+          text={displayName}
+          button={SingleDeleteButton({ imageName: imagename })}
+        />
+        <Image imageName={imagename} />
+      </>
+    );
+  } else {
+    component = (
+      <>
+        <ImageList />
+        <div className="flex items-center justify-center w-full my-16">
+          <LogoutButton />
+        </div>
+      </>
+    );
+  }
   return (
     <>
       <Navbar />
-      {loggedIn ? (
-        <>
-          <ImageList />
-          <div className="flex items-center justify-center w-full my-16">
-            <LogoutButton />
-          </div>
-        </>
-      ) : (
-        <LoginButton />
-      )}
+      {loggedIn ? component : <LoginButton />}
     </>
   );
 }
