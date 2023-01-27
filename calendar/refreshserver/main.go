f6d88d2b7ab69f794045ff9fbcc09df294807e5d
FantomeBeignet <himself@fantomebeig.net>
2023-01-27 18:11:31 +0100

    feat: start of refresh server

--- calendar/refreshserver/main.go
+++ calendar/refreshserver/main.go
@@ -1,1 +1,17 @@
 package main
+
+import "net/http"
+
+func refresh(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPut {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	} else {
+		w.Write([]byte("Refresh OK"))
+	}
+}
+
+func main() {
+	http.HandleFunc("/refresh", refresh)
+	http.ListenAndServe(":3333", nil)
+}
