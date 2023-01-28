0ed1b74377636a89d35c691e8ccb838c58251318
FantomeBeignet <himself@fantomebeig.net>
2023-01-28 13:37:43 +0100

    feat: implemented refresh server

--- calendar/refreshserver/main.go
+++ calendar/refreshserver/main.go
@@ -1,13 +1,23 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+	"os/exec"
+)
 
 func refresh(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPut {
 		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 		return
 	} else {
-		w.Write([]byte("Refresh OK"))
+		cmd := exec.Command("/app/calendar", "manual")
+		err := cmd.Run()
+		if err != nil {
+			http.Error(w, "Refresh failed", http.StatusInternalServerError)
+			return
+		} else {
+			w.Write([]byte("Refresh OK"))
+		}
 	}
 }
 
