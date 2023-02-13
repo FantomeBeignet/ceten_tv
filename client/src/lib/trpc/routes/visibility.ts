1bb82cbc6cb8509e259053378aeba8e1facaadfc
FantomeBeignet <himself@fantomebeig.net>
2023-02-13 15:09:44 +0100

    style: formatting

--- client/src/lib/trpc/routes/visibility.ts
+++ client/src/lib/trpc/routes/visibility.ts
@@ -4,11 +4,11 @@ import redisClient from '$lib/redis';
 
 export const visibility = t.router({
 	show: t.procedure.input(z.string()).mutation(async ({ input }) => {
-        const res = await redisClient.sMove('hidden', 'visible', input);
-        return res;
-    }),
-    hide: t.procedure.input(z.string()).mutation(async ({ input }) => {
-        const res = await redisClient.sMove('visible', 'hidden', input);
-        return res;
-    }),
+		const res = await redisClient.sMove('hidden', 'visible', input);
+		return res;
+	}),
+	hide: t.procedure.input(z.string()).mutation(async ({ input }) => {
+		const res = await redisClient.sMove('visible', 'hidden', input);
+		return res;
+	})
 });
