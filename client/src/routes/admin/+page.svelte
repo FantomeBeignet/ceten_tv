b655aae83da3d8019f6f848f41d70f0fe57a7fc6
FantomeBeignet <himself@fantomebeig.net>
2023-02-13 16:35:22 +0100

    feat: base for admin page

---
+++ client/src/routes/admin/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+	import { page } from '$app/stores';
+	import { signIn } from '@auth/sveltekit/client';
+</script>
+
+{#if Object.keys($page.data.session || {}).length}
+	<h1>Coucou</h1>
+{:else}
+	<button on:click={signIn}>Sign in</button>
+{/if}
