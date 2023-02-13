925670aae1791e3b7e5f59120f343b57fcc45f05
FantomeBeignet <himself@fantomebeig.net>
2023-02-13 15:04:12 +0100

    feat: trpc route to update image visibility

---
+++ client/src/lib/trpc/routes/visibility.ts
@@ -0,0 +1,14 @@
+import { t } from '$lib/trpc/t';
+import { z } from 'zod';
+import redisClient from '$lib/redis';
+
+export const visibility = t.router({
+	show: t.procedure.input(z.string()).mutation(async ({ input }) => {
+        const res = await redisClient.sMove('hidden', 'visible', input);
+        return res;
+    }),
+    hide: t.procedure.input(z.string()).mutation(async ({ input }) => {
+        const res = await redisClient.sMove('visible', 'hidden', input);
+        return res;
+    }),
+});
