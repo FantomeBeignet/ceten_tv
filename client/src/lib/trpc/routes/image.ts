934d2f9a2ec165e1ae4c2f0d963dc72df978ae6d
FantomeBeignet <himself@fantomebeig.net>
2023-02-16 16:57:35 +0100

    feat: checking input image name is a uuid

--- client/src/lib/trpc/routes/image.ts
+++ client/src/lib/trpc/routes/image.ts
@@ -3,38 +3,38 @@ import { z } from 'zod';
 import redisClient from '$lib/redis';
 
 export const image = t.router({
-	add: t.procedure.input(z.string()).mutation(async ({ input }) => {
+	add: t.procedure.input(z.string().uuid()).mutation(async ({ input }) => {
 		const res = await redisClient.zadd('visible', '+inf', input);
 		return res;
 	}),
-	remove: t.procedure.input(z.string()).mutation(async ({ input }) => {
+	remove: t.procedure.input(z.string().uuid()).mutation(async ({ input }) => {	
 		const res = await redisClient.zrem('visible', input);
 		return res;
 	}),
-	show: t.procedure.input(z.string()).mutation(async ({ input }) => {
+	show: t.procedure.input(z.string().uuid()).mutation(async ({ input }) => {
 		const res = await redisClient.zadd('visible', '+inf', input);
 		return res;
 	}),
 	showUntil: t.procedure
-		.input(z.object({ uuid: z.string(), date: z.number().int() }))
+		.input(z.object({ uuid: z.string().uuid(), date: z.number().int() }))
 		.mutation(async ({ input }) => {
 			const res = await redisClient.zadd('visible', input.date, input.uuid);
 			return res;
 		}),
-	hide: t.procedure.input(z.string()).mutation(async ({ input }) => {
+	hide: t.procedure.input(z.string().uuid()).mutation(async ({ input }) => {
 		const res = await redisClient.zadd('visible', '-inf', input);
 		return res;
 	}),
-	getAll: t.procedure.input(z.string()).query(async () => {
+	getAll: t.procedure.input(z.string().uuid()).query(async () => {
 		const res = await redisClient.zrange('visible', 0, -1);
 		return res;
 	}),
-	getVisible: t.procedure.input(z.string()).query(async () => {
+	getVisible: t.procedure.input(z.string().uuid()).query(async () => {
 		const now = Date.now();
 		const res = await redisClient.zrangebyscore('visible', now, '+inf');
 		return res;
 	}),
-	getHidden: t.procedure.input(z.string()).query(async () => {
+	getHidden: t.procedure.input(z.string().uuid()).query(async () => {
 		const now = Date.now();
 		const res = await redisClient.zrangebyscore('visible', '-inf', `(${now}`);
 		return res;
