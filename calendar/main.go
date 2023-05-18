1b0e86d90fe052307af08b6677150d9d38e3622d
Tom Béné <himself@fantomebeig.net>
2023-05-18 23:03:03 +0200

    feat: api route to refresh calendar

--- calendar/main.go
+++ calendar/main.go
@@ -2,11 +2,17 @@ package main
 
 import (
 	"log"
+	"net/http"
 	"os"
 	"time"
 
 	"github.com/go-co-op/gocron"
+	"github.com/julienschmidt/httprouter"
 )
+
+func RefreshFromRequest(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	RefreshCalendar()
+}
 
 func main() {
 	logfile, err := os.OpenFile("/app/logs/agenda.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
@@ -19,4 +25,8 @@ func main() {
 	s := gocron.NewScheduler(time.UTC)
 	s.Every(1).Days().At("7:00;12:00;17:00").Do(RefreshCalendar)
 	s.StartAsync()
+
+	router := httprouter.New()
+	router.GET("/", RefreshFromRequest)
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
